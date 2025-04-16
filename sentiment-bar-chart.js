b90a5e025fa4049bc0f77fa1506474981e0a0d0e
nyctar <115890540+nyctar@users.noreply.github.com>
2025-04-16 09:06:41 -0400

    added html, js, and json for data viz

---
+++ sentiment-bar-chart.js
@@ -0,0 +1,46 @@
+d3.json("sentiment-bar-chart.json").then(data => {
+    data.forEach(d => {
+        d.index_by_10 = +d.index_by_10;
+        d.sentiment = +d.sentiment;
+        d.positive = d.sentiment > 0;
+    });
+
+    const margin = { top: 30, right: 20, bottom: 40, left: 50 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("#sentimentBarChart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleBand()
+        .domain(data.map(d => d.index_by_10))
+        .range([0, width])
+        .padding(0.1);
+
+    const y = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.sentiment)).nice()
+        .range([height, 0]);
+
+    const color = d => d.positive ? "#2ca02c" : "#d62728";
+
+    svg.append("g")
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", d => x(d.index_by_10))
+        .attr("y", d => d.sentiment >= 0 ? y(d.sentiment) : y(0))
+        .attr("height", d => Math.abs(y(d.sentiment) - y(0)))
+        .attr("width", x.bandwidth())
+        .attr("fill", color);
+
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", -10)
+        .attr("text-anchor", "middle")
+        .style("font-size", "18px")
+});
---
+++ word-cloud.js
@@ -0,0 +1,30 @@
+d3.json("word-cloud.json").then(function(words) {
+const width = 500;
+const height = 400;
+
+const layout = d3.layout.cloud()
+  .size([width, height])
+  .words(words.map(d => Object.create(d)))
+  .padding(5)
+  .rotate(() => (~~(Math.random() * 2)) * 90)
+  .fontSize(d => d.size)
+  .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+  d3.select("#wordCloud").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${width / 2},${height / 2})`)
+    .selectAll("text")
+    .data(words)
+    .enter().append("text")
+    .style("font-size", d => d.size + "px")
+    .style("fill", () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
+    .attr("text-anchor", "middle")
+    .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
+    .text(d => d.text);
+}
+});
