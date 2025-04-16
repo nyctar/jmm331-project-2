dac5d5d0ab6e1c2064c0144a43b6a0263f490341
nyctar <115890540+nyctar@users.noreply.github.com>
2025-04-16 10:09:56 -0400

    changed colors

--- word-cloud/word-cloud.js
+++ word-cloud/word-cloud.js
@@ -1,30 +1,35 @@
 d3.json("word-cloud.json").then(function(words) {
-const width = 500;
-const height = 400;
+  const width = 500;
+  const height = 400;
 
-const layout = d3.layout.cloud()
-  .size([width, height])
-  .words(words.map(d => Object.create(d)))
-  .padding(2)
-  .rotate(() => (~~(Math.random() * 2)) * 90)
-  .fontSize(d => d.size * 0.6)
-  .on("end", draw);
+  const colorScale = d3.scaleLinear()
+    .domain([0, words.length - 1])
+    .range(["#0E79B2", "#F26419"])
+    .interpolate(d3.interpolateRgb);
 
-layout.start();
+  const layout = d3.layout.cloud()
+    .size([width, height])
+    .words(words.map(d => Object.create(d)))
+    .padding(2)
+    .rotate(() => (~~(Math.random() * 2)) * 90)
+    .fontSize(d => d.size * 0.6)
+    .on("end", draw);
 
-function draw(words) {
-  d3.select("#wordCloud").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", `translate(${width / 2},${height / 2})`)
-    .selectAll("text")
-    .data(words)
-    .enter().append("text")
-    .style("font-size", d => d.size + "px")
-    .style("fill", () => d3.schemeCategory10[Math.floor(Math.random() * 10)])
-    .attr("text-anchor", "middle")
-    .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
-    .text(d => d.text);
-}
+  layout.start();
+
+  function draw(words) {
+    d3.select("#wordCloud").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2},${height / 2})`)
+      .selectAll("text")
+      .data(words)
+      .enter().append("text")
+      .style("font-size", d => d.size + "px")
+      .style("fill", (d, i) => colorScale(i))
+      .attr("text-anchor", "middle")
+      .attr("transform", d => `translate(${d.x},${d.y}) rotate(${d.rotate})`)
+      .text(d => d.text);
+  }
 });
